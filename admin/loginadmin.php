c3d0888755c79859cba53d81721dd413ca47d5b5
geraldarko <gerald.darko@ashesi.edu.gh>
2021-12-10 22:37:56 +0000

    Fixed admin page

--- admin/loginadmin.php
+++ admin/loginadmin.php
@@ -14,6 +14,7 @@
 </head>
 <body>
     <?php 
+        session_start();
         require_once("../controllers/users_controller.php");
         if (isset($_POST['loginbtn'])) {
             $credential = $_POST['credential'];
--- admin/show.php
+++ admin/show.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 if(!isset($_SESSION['admin'])){
 	header("Location: loginadmin.php");
 }
